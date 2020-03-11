98243f8475141899b344033a3161a2a84e6de080
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-03-11 10:16:36 -0400

    got props added to the locations container

--- src/App.js
+++ src/App.js
@@ -2,13 +2,38 @@ import React from 'react';
 import './App.css';
 import HomePage from './containers/HomePage';
 import LocationsContainer from './containers/LocationsContainer'
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './containers/NavBar'
 
 class App extends React.Component {
+
+  state = {
+    loading: true,
+    locationsArray: []
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/locations")
+    .then(res => res.json())
+    .then(data=>this.setState({locationsArray: data}))
+  }
+
   render(){
     return(
       <div className = "app">
-        <HomePage />
-        <LocationsContainer/>
+        <Router>
+          <NavBar/>
+
+          <Route exact path="/" render={props=> (
+            <HomePage />
+          )}/>
+
+          <Route path = "/locations" render={props => (
+                 <LocationsContainer allLocations={this.state.locationsArray}/>
+          )}/>
+
+        </Router>
       </div>
     )
 
